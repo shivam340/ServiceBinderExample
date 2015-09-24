65ba7566a4e6cca8374ae01ea9b620cdd4e36963
shivam340 <shivam@leftshift.io>
2015-09-24 15:56:27 +0530

    added handler in MainActivity so using it we can pass messages to main threads message queue

--- app/src/main/java/bind/com/servicebinderexample/MainActivity.java
+++ app/src/main/java/bind/com/servicebinderexample/MainActivity.java
@@ -6,22 +6,29 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private UnLeashedService mUnLeashedService = null;
     private boolean mIsBound = false;
-
+    private Handler mHandler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "MainActivity Thread Id is : "  + Thread.currentThread().getId());
         showDialog();
+        createHandler();
     }
 
 
@@ -66,8 +73,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    private void showDialog(){
+    private void showDialog() {
         AlertDialog.Builder alertDialogBuilder =
                 new AlertDialog.Builder(this);
 
@@ -95,6 +101,53 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.setCancelable(false);
         alertDialog.show();
+
+    }
+
+
+    /**
+     * Creates handler for main Thread's message queue.
+     * It will allow background thread to send messages to this (main/ui) thread.
+     */
+
+    private void createHandler() {
+
+        mHandler = new Handler() {
+
+            @Override
+            public void handleMessage(Message message) {
+                super.handleMessage(message);
+
+                Log.d(TAG, "in handleMessage().");
+                Log.d(TAG, "MainActivity (Handler) Thread Id is : "  + Thread.currentThread().getId());
+
+                if (message != null) {
+                    Bundle data = message.getData();
+
+                    if (data != null && data.getBoolean("download_completed")) {
+                        Toast.makeText(getApplicationContext(), "Download completed.", Toast
+                                .LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Download failed.", Toast
+                                .LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Log.d(TAG, "no data in handleMessage().");
+                }
+
+            }
+        };
+
+        // started background thread to do some long running task.
+        // and passed handler of main thread so that background thread can send messages to main thread using it's handler
+        new MyThread(mHandler).start();
+
+    }
+
+
+    public Handler getHandler() {
+        return mHandler;
     }
 
 }
